d3c54d4e06095fbf8fb1d636235cac4986a2ef2d
LouisGrey7 <100319006+LouisGrey7@users.noreply.github.com>
2022-06-03 19:58:59 +1200

    level up system partially implemented

--- Project Mustard Cars/Assets/Scripts/Damagable.cs
+++ Project Mustard Cars/Assets/Scripts/Damagable.cs
@@ -8,13 +8,11 @@ public class Damagable : MonoBehaviour
     public int MaxHealth = 100;
     [SerializeField]
     private int health;
-    
-    
-    
-    
+
+
     public int Health
     {
-        get { return health;  }
+        get { return health; }
         set
         {
             health = value;
@@ -28,9 +26,6 @@ public class Damagable : MonoBehaviour
     public UnityEvent OnHit, OnHeal;
 
 
-
-    
-    
     // Start is called before the first frame update
     private void Start()
     {
@@ -44,6 +39,7 @@ public class Damagable : MonoBehaviour
         {
             OnDead?.Invoke();
             Destroy(gameObject);
+            
         }
         else
         {
