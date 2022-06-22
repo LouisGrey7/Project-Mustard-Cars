a33bcc6464cf7ab8cfd9aa4973b0bdac4ece77ec
clee101 <100319177+clee101@users.noreply.github.com>
2022-06-23 00:06:27 +1200

    Explody bois

--- Project Mustard Cars/Assets/Scripts/Damagable.cs
+++ Project Mustard Cars/Assets/Scripts/Damagable.cs
@@ -5,6 +5,7 @@ using UnityEngine.Events;
 
 public class Damagable : MonoBehaviour
 {
+
     private GameObject player;
     LevelSystem playerLevel;
     public int MaxHealth = 100;
@@ -12,6 +13,7 @@ public class Damagable : MonoBehaviour
     private int health;
     public bool deadObject = false;
     public AudioSource bulletHitSFX;
+    public GameObject deathEffect;
 
     public int Health
     {
@@ -39,6 +41,8 @@ public class Damagable : MonoBehaviour
         Health = MaxHealth;
         player = GameObject.FindGameObjectWithTag("Player");
         playerLevel = player.GetComponent<LevelSystem>();
+        
+
     }
 
     internal void Hit(int damagePoints)
@@ -47,9 +51,10 @@ public class Damagable : MonoBehaviour
         Health -= damagePoints;
         if (health <= 0)
         {
-            
+
+            DeathEffect();
             OnDead?.Invoke();
-            Destroy(gameObject);
+
             playerLevel.GainExperienceScalable(2, playerLevel.level);
             deadObject = true;
         }
@@ -65,6 +70,24 @@ public class Damagable : MonoBehaviour
         Health += healthBoost;
         Health = Mathf.Clamp(Health, 0, MaxHealth);
         OnHeal?.Invoke();
+    }
+
+    private void DeathEffect()
+    {
+        if(deathEffect != null)
+        {
+            GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 1f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            GameObject effect = Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(effect, 1f);
+        }
     }
 
 }
